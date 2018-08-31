cfa5956d7e06a3082b1886dd209821e9c934aa50
takashima-dream-v <takashima.dreamvision@gmail.com>
2018-09-01 04:04:25 +0900

    テンプレート修正 レイアウト調整

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/fixed.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/fixed.php
@@ -6,6 +6,12 @@ Template Name: 固定ページ
 <!-- ヘッダー -->
 <?php get_header(); ?>
 
+<?php
+if(have_posts()): while(have_posts()): the_post();?>
+
+    <?php the_content(); ?>
+
+<?php endwhile; endif; ?>
 
 <!--　フッター　-->
 <?php get_footer(); ?>
