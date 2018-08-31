37ea1e1000ed09bbc56eb14bf01ae1479c2e177e
takashima-dream-v <takashima.dreamvision@gmail.com>
2018-09-01 04:24:46 +0900

    テンプレート修正

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/fixed.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/fixed.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: 固定ページ
+Template Name: お問い合わせページ
 */
 ?>
 <!-- ヘッダー -->
@@ -9,9 +9,10 @@ Template Name: 固定ページ
 <?php
 if(have_posts()): while(have_posts()): the_post();?>
 
-    <?php remove_filter(‘the_content’, ‘wpautop’); ?>
     <div class="fixed-bg">
+        <?php remove_filter(‘the_content’, ‘wpautop’); ?>
         <?php the_content(); ?>
+        <div style=“width:100%;height:600px;overflow:auto;-webkit-overflow-scrolling:touch;“><iframe src=“https://tayori.com/form/078ee5a1e0088817f71e52826b33aeaa32485dda” width=“100%” height=“100%“></iframe></div>
     </div>
 <?php endwhile; endif; ?>
 
