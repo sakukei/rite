969d0a2c298a8f7b15433a8e01feefe6f5c20c8c
takashima-dream-v <takashima.dreamvision@gmail.com>
2018-09-05 02:32:15 +0900

    アーカイブとカテゴリーページの文字数制限実装

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/archive.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/archive.php
@@ -39,9 +39,23 @@ get_header(); ?>
 								<?php endif; ?>
 								<div class="post-info-wrap">
                                     <div class="post-character">
-                                        <div class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+                                        <div class="post-title"><a href="<?php the_permalink(); ?>"><?php
+                                                if(mb_strlen($post->post_title)>24) {
+                                                    $title= mb_substr($post->post_title,0,24) ;
+                                                    echo $title . '...';
+                                                } else {
+                                                    echo $post->post_title;
+                                                }
+                                                ?></a></div>
                                         <?php remove_filter('the_excerpt',array($usces,'filter_cartContent'),20); ?>
-                                        <div class="post-excerpt"><?php the_excerpt(); ?></div>
+                                        <div class="post-excerpt"><?php
+                                            if(mb_strlen($post->post_content,'UTF-8')>70){
+                                                $content= str_replace('\n', '', mb_substr(strip_tags($post-> post_content), 0, 70,'UTF-8'));
+                                                echo $content.'……';
+                                            }else{
+                                                echo str_replace('\n', '', strip_tags($post->post_content));
+                                            }
+                                            ?></div>
                                     </div>
                                     <!--<div class="post-cat">--><?php //the_category(',') ?><!--</div>-->
                                     <!--<div class="post-date">--><?php //the_date(); ?><!--</div>-->
