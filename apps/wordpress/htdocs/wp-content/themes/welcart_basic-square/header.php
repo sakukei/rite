c3d43c570b3f45b094fa58bf11c70aa71b5193d7
takashima-dream-v <takashima.dreamvision@gmail.com>
2018-09-07 00:48:03 +0900

    SPメニューの修正

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/header.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/header.php
@@ -119,14 +119,14 @@
 <!--                            <li><a href="--><?php //get_stylesheet_directory_uri(); ?><!--/tag/hongkong"><img src="--><?php //echo get_template_directory_uri(); ?><!--/images/sidebar-hongkong.png" alt="">香港</a></li>-->
 <!--                            <li><a href="--><?php //get_stylesheet_directory_uri(); ?><!--/tag/taiwan"><img src="--><?php //echo get_template_directory_uri(); ?><!--/images/sidebar-taiwan.png" alt="">台湾</a></li>-->
                             <li><a href="<?php get_stylesheet_directory_uri(); ?>/tag/korea"><img src="<?php echo get_template_directory_uri(); ?>/images/sidebar-korea.png" alt="">韓国</a></li>
-                            <li><a href="<?php get_stylesheet_directory_uri(); ?>/tag/japan"><img src="<?php echo get_template_directory_uri(); ?>/images/sidebar-japan.png" alt="">日本</a></li>
+                            <li><a href="<?php get_stylesheet_directory_uri(); ?>/tag/bali"><img src="<?php echo get_template_directory_uri(); ?>/images/sidebar-bali.png" alt=""><span>バリ</span></a></li>
                         </ul>
                     </section>
                     <section class="sidebar sidebar-contributor">
                         <h3 class="sidebar-title">たびびと検索</h3>
                         <ul class="sidebar-list">
                             <li><a href="<?php get_stylesheet_directory_uri(); ?>/category/ayasuke_0516/"><img src="<?php echo get_template_directory_uri(); ?>/images/icon_hasegawa_aya.jpeg" alt=""><span>長谷川あや</span></a></li>
-<!--                            <li><a href="--><?php //get_stylesheet_directory_uri(); ?><!--/tag/yoshino_hitomi">良野仁美</a></li>-->
+                            <li><a href="<?php get_stylesheet_directory_uri(); ?>/tag/hitostagram12"><img src="<?php echo get_template_directory_uri(); ?>/images/icon_yoshino_hitomi.jpeg" alt=""><span>良野仁美</span></a></li>
                         </ul>
                     </section>
                 </div>
