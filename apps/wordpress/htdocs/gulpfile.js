11a5bdc1201ef4205e0ef245f193827026d9ceb1
kango furusu <furusu0715@gmail.com>
2018-12-13 14:40:43 +0900

    gulp css sass browsersync

---
+++ apps/wordpress/htdocs/gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync   = require('browser-sync');
+const cmq = require('gulp-combine-media-queries');
+const plumber = require('gulp-plumber');
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sassGlob = require("gulp-sass-glob");
+const connect = require('gulp-connect-php');
+
+gulp.task('sass', () => {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    }))
+    .pipe(sassGlob())
+    // Sassのコンパイルを実行
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(cmq())
+    .pipe(postcss([
+      autoprefixer({
+        // IEは11以上、Androidは4.4以上
+        // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
+        browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
+        cascade: false,
+        grid: 'autoplace'
+      }),
+    ]))
+    .pipe(gulp.dest('./src/css'))
+});
+
+
+gulp.task('connect-sync', function() {
+    return connect.server({
+        base:'/'
+    }, function (){
+        browserSync({
+            proxy: 'localhost'
+        });
+    });
+});
+
+gulp.task('reload', function(){
+    browserSync.reload();
+});
+
+gulp.task('default',['connect-sync'], function() {
+    gulp.watch('src/sass/**/*.scss',['sass']);
+    gulp.watch('src/css/**/*.css',['reload']);
+});
