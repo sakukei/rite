257f75d3c6d46518b7072332400cf5878fbf2d4c
kango furusu <furusu0715@gmail.com>
2018-12-26 17:26:54 +0900

    scssファイルのパスがおかしくなってたので修正

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/footer.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/footer.php
@@ -62,7 +62,8 @@
 
 	</div><!-- wrapper -->
 
-		
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.js"></script>
+            <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 	<?php wp_footer(); ?>
             <script>
                 jQuery(function($) {
---
+++ apps/wordpress/src/es6/scripts.js
@@ -0,0 +1,31 @@
+Vue.component('testAxios', {
+    props: ['theUrl'],
+    data () {
+        return {
+            info: '',
+        }
+    },
+    mounted () {
+        axios
+            .get(this.theUrl)
+            .then(response => (this.info = response.data))
+    },
+    template: `<article>
+    <section v-for="item in info">
+      <h2>{{ item.title.rendered }}</h2>
+      <p v-html="item.excerpt.rendered"></p>
+      <time>{{ item.date.slice(0, 10) }}</time>
+    </section>
+    </article>`,
+});
+
+new Vue({
+    el: '#app',
+    data: function() {
+        const baseUrl = location.origin;
+        return {
+            theUrlAlpha: `${baseUrl}/wp-json/wp/v2/posts/`,
+            theUrlBravo: `${baseUrl}/wp-json/wp/v2/posts/`,
+        }
+    },
+});
