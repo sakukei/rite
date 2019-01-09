0969d58192ebe608f230242d5f741593d2dd23e3
kango furusu <furusu0715@gmail.com>
2019-01-09 13:00:05 +0900

    webpack vue設定追加

--- apps/wordpress/htdocs/webpack.config.js
+++ apps/wordpress/htdocs/webpack.config.js
@@ -1,6 +1,34 @@
-const path = require('path')
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/, // ファイルが.vueで終われば...
+        loader: 'vue-loader' // vue-loaderを使う
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'] // css-loader -> vue-style-loaderの順で通していく
+      },
+    ]
+  },
+  resolve: {
+    // import './foo.vue' の代わりに import './foo' と書けるようになる(拡張子省略)
+    extensions: ['.js', '.vue'],
+    alias: {
+      // vue-template-compilerに読ませてコンパイルするために必要
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   // エントリポイントのファイル
   entry: './src/es6/index.js',
   output: {
