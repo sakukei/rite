fbbc513487f161155c9e6cf1254e198345752844
takashima-dream-v <takashima.dreamvision@gmail.com>
2019-01-12 23:41:42 +0900

    記事ページと商品ページのテンプレートのレイアウト調整

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/single.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/single.php
@@ -5,59 +5,63 @@
  */
 
 get_header(); ?>
-    <!--　TOP2カラム　-->
-    <div class="contents-column">
+  <!--　TOP2カラム　-->
+
+// 記事の自動整形を無効にする
+<?php remove_filter('the_content', 'wpautop'); ?>
+// 抜粋の自動整形を無効にする
+<?php remove_filter( 'the_excerpt', 'wpautop' ); ?>
+  <div class="contents-column">
 
     <div id="primary" class="site-content">
-        <div id="content" class="cf" role="main">
+      <div id="content" class="cf" role="main">
 
-            <div class="column-wrap">
+        <div class="column-wrap">
 
-                <div class="column">
+          <div class="column">
 
-                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                        <?php get_template_part('template-parts/content', get_post_format()); ?>
-                        <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
+              <?php get_template_part('template-parts/content', get_post_format()); ?>
+              <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
 
-                        <?php if (!usces_is_item()): ?>
+              <?php if (!usces_is_item()): ?>
 
-                            <div class="comment-area">
-                                <div class="feedback">
-                                    <?php wp_link_pages(); ?>
-                                </div>
-                                <!--						--><?php //comments_template( '', true ); ?>
-                            </div><!-- .comment-area -->
+                <div class="comment-area">
+                  <div class="feedback">
+                    <?php wp_link_pages(); ?>
+                  </div>
+                  <!--						--><?php //comments_template( '', true ); ?>
+                </div><!-- .comment-area -->
 
-                        <?php endif; ?>
+              <?php endif; ?>
 
-                    <?php endwhile; else: ?>
+            <?php endwhile; else: ?>
 
-                        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+              <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 
-                    <?php endif; ?>
+            <?php endif; ?>
 
-                </div><!-- column -->
-            </div><!-- column-wrap -->
-
-
-            <?php
-                $args = array(
-                    // 関連記事を表示する最大件数
-                    'limit'    => 10,
-                    // 使用するテンプレートの名前を指定
-                    'template' => 'yarpp-template-relative.php',
-                );
-                yarpp_related($args);
-            ?>
-
-            <!--			--><?php //get_sidebar('other'); ?>
+          </div><!-- column -->
+        </div><!-- column-wrap -->
 
 
+        <?php
+        $args = array(
+          // 関連記事を表示する最大件数
+          'limit' => 10,
+          // 使用するテンプレートの名前を指定
+          'template' => 'yarpp-template-relative.php',
+        );
+        yarpp_related($args);
+        ?>
 
-        </div><!-- #content -->
+        <!--			--><?php //get_sidebar('other'); ?>
+
+
+      </div><!-- #content -->
     </div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-    </div>
+    <?php get_sidebar(); ?>
+  </div>
 <?php get_footer(); ?>
