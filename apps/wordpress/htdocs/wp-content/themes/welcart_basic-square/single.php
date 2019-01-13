6efc913c8fa35ad733d3c6c4777a10c55af23c45
takashima-dream-v <takashima.dreamvision@gmail.com>
2019-01-13 18:15:12 +0900

    インフルエンサーINFOの作成

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/single.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/single.php
@@ -5,63 +5,66 @@
  */
 
 get_header(); ?>
-  <!--　TOP2カラム　-->
+<!--　TOP2カラム　-->
 
 // 記事の自動整形を無効にする
 <?php remove_filter('the_content', 'wpautop'); ?>
 // 抜粋の自動整形を無効にする
-<?php remove_filter( 'the_excerpt', 'wpautop' ); ?>
-  <div class="contents-column">
+<?php remove_filter('the_excerpt', 'wpautop'); ?>
+<div class="contents-column">
 
-    <div id="primary" class="site-content">
-      <div id="content" class="cf" role="main">
+  <div id="primary" class="site-content">
+    <div id="content" class="cf" role="main">
 
-        <div class="column-wrap">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-          <div class="column">
+        <?php get_template_part('template-parts/content', get_post_format()); ?>
+        <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
 
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php if (!usces_is_item()): ?>
 
-              <?php get_template_part('template-parts/content', get_post_format()); ?>
-              <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
+          <div class="comment-area">
+            <div class="feedback">
+              <?php wp_link_pages(); ?>
+            </div>
+            <!--						--><?php //comments_template( '', true ); ?>
+          </div><!-- .comment-area -->
 
-              <?php if (!usces_is_item()): ?>
+        <?php endif; ?>
 
-                <div class="comment-area">
-                  <div class="feedback">
-                    <?php wp_link_pages(); ?>
-                  </div>
-                  <!--						--><?php //comments_template( '', true ); ?>
-                </div><!-- .comment-area -->
+      <?php endwhile; else: ?>
 
-              <?php endif; ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 
-            <?php endwhile; else: ?>
+      <?php endif; ?>
 
-              <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-
-            <?php endif; ?>
-
-          </div><!-- column -->
-        </div><!-- column-wrap -->
-
+      <div class="p-user">
+        <div class="p-user-iconWrap">
+          <?php echo get_avatar(get_the_author_meta('ID'), '','', '', array('class' => 'c-icon-user p-user-icon')); ?>
+        </div>
+        <p class="p-user_name"><?php echo get_the_author(); ?></p>
+        <p class="p-user_description"><?php the_author_meta('description'); ?></p>
+        <p class="c-account p-user_account">
+          <a href="<?php the_author_meta('user_url'); ?>" class="c-account_link"><?php the_author_meta('nickname'); ?></a>
+        </p>
+      </div>
 
         <?php
-        $args = array(
-          // 関連記事を表示する最大件数
-          'limit' => 10,
-          // 使用するテンプレートの名前を指定
-          'template' => 'yarpp-template-relative.php',
-        );
-        yarpp_related($args);
-        ?>
+      $args = array(
+        // 関連記事を表示する最大件数
+        'limit' => 10,
+        // 使用するテンプレートの名前を指定
+        'template' => 'yarpp-template-relative.php',
+      );
+      yarpp_related($args);
+      ?>
 
-        <!--			--><?php //get_sidebar('other'); ?>
+      <!--			--><?php //get_sidebar('other'); ?>
 
 
-      </div><!-- #content -->
-    </div><!-- #primary -->
+    </div><!-- #content -->
+  </div><!-- #primary -->
 
-    <?php get_sidebar(); ?>
-  </div>
+<!--  --><?php //get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
