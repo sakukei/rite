bce91f467c7191cbe3b9741f20abe8a61514a426
kango furusu <furusu0715@gmail.com>
2019-01-19 20:01:17 +0900

    App.vueで取得してみる

--- apps/wordpress/htdocs/src/js/App.vue
+++ apps/wordpress/htdocs/src/js/App.vue
@@ -10,11 +10,25 @@
       <router-link to ='/all'>All</router-link>
     </nav>
     <router-view />
+    {{posts}}
   </div>
 </template>
 
 <script>
-
+  import axios from 'axios';
+  export default {
+    data: function() {
+      return {
+        posts: ''
+      };
+    },
+    mounted() {
+      const baseUrl = location.origin;
+      axios
+        .get(`${baseUrl}/wp-json/wp/v2/posts`)
+        .then(response => (this.posts = response.data));
+    },
+  }
 </script>
 
 <style scoped lang="scss">
--- apps/wordpress/htdocs/src/js/views/Pickup.vue
+++ apps/wordpress/htdocs/src/js/views/Pickup.vue
@@ -2,32 +2,32 @@
   <div class="test">
     <h1>Pickup</h1>
     <ul>
-      <li v-for="item in pickup.data" :key="item.id">
-        <a :href="item.link">
+      <!--<li v-for="item in pickup.data" :key="item.id">-->
+        <!--<a :href="item.link">-->
 
-          {{item.title.rendered}}
-          <img :src="item._embedded['wp:featuredmedia'][0].source_url" alt="">
-        </a>
-      </li>
+          <!--{{item.title.rendered}}-->
+          <!--<img :src="item._embedded['wp:featuredmedia'][0].source_url" alt="">-->
+        <!--</a>-->
+      <!--</li>-->
     </ul>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
-  export default {
-    data: function() {
-      return {
-        pickup: ''
-      };
-    },
-    mounted() {
-      const baseUrl = location.origin;
-      axios
-        .get(`${baseUrl}/wp-json/wp/v2/top_pickups?_embed`)
-        .then(response => (this.pickup = response));
-    },
-  }
+  // import axios from 'axios'
+  // export default {
+  //   data: function() {
+  //     return {
+  //       pickup: ''
+  //     };
+  //   },
+  //   mounted() {
+  //     const baseUrl = location.origin;
+  //     axios
+  //       .get(`${baseUrl}/wp-json/wp/v2/`)
+  //       .then(response => (this.pickup = response));
+  //   },
+  // }
 </script>
 
 <style scoped>
