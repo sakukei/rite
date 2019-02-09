dbbdbd102757d0b76a9607350afc08d5b3ff70af
kango furusu <furusu0715@gmail.com>
2019-02-09 23:54:55 +0900

    トラベラーページのエラー解消

--- apps/wordpress/htdocs/src/js/views/Traveler.vue
+++ apps/wordpress/htdocs/src/js/views/Traveler.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="l-inner">
-    <section class="p-grid-section">
+    <section class="p-grid-section traveler-section" v-bind:class="{'is-hidden': display}">
       <div class="p-traveler-name -nami">なみ</div>
       <ul class="p-sub-grid">
         <li v-for="item in nami" :key="item.id">
@@ -8,45 +8,56 @@
             <img :src="item.featured_image.src" class="c-grid-img"/>
           </a>
         </li>
-        <!--<li>-->
-          <!--<a :href="namiLink.link">-->
-            <!--<img :src="namiLast.featured_image.src" />-->
-            <!--<div class="p-more"><span>もっとみる</span></div>-->
-          <!--</a>-->
-        <!--</li>-->
+        <li>
+          <a :href="namiLink">
+            <img :src="namiLast" />
+            <div class="p-more"><span>もっとみる</span></div>
+          </a>
+        </li>
       </ul>
-      <!--{{namiLink.link}}-->
     </section>
   </div>
 </template>
 
 <script>
   export default {
+    data(){
+      return {
+        namiLast: '',
+        namiLink: 'default',
+        display: true
+      }
+    },
     computed: {
-      namiLast() {
-        console.log('a')
-        return this.$store.state.namis.slice(0,5);
-      },
       nami() {
-        console.log('e')
-        return this.$store.state.namis.slice(0,5);
+        const posts = this.$store.state.namis;
+        return posts;
       },
-      namiLink(){
+    },
+    mounted() {
+        this.$store.dispatch('getCategory').then((res)=>{
+        this.$store.commit('getCategory', res.data )
         const nami = this.$store.state.categories.find(function(category){
           return category.name === 'なみ';
         });
-        return nami;
-      }
-    },
-    mounted() {
+        this.namiLink = nami.link
+      })
       this.$store.dispatch('getNami').then((res)=>{
         this.$store.commit('getNami', res.data )
+        this.namiLast = this.$store.state.namis.pop().featured_image.src;
+        this.display = false;
       });
     },
   }
 </script>
 
 <style scoped lang="scss">
+  .traveler-section {
+    &.is-hidden {
+    display: none
+    }
+
+  }
   .p-sub-grid {
     display: grid;
     grid-template: repeat(3,1fr) / repeat(3,1fr);
