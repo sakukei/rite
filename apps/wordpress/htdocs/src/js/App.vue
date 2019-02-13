6ca68fa9cb4e3f9d228fa6dd47e9898a8531e24c
takashima-dream-v <takashima.dreamvision@gmail.com>
2019-02-13 23:33:28 +0900

    カテゴリーの順番と名前を修正

--- apps/wordpress/htdocs/src/js/App.vue
+++ apps/wordpress/htdocs/src/js/App.vue
@@ -2,13 +2,13 @@
   <div>
     <nav class="p-category-nav">
       <ul class="p-category-list">
-        <li><router-link class="-pickup" to ='/'><span class="p-category-list-name">Pickup</span></router-link></li>
-        <li><router-link class="-traveler" to ='/traveler'><span class="p-category-list-name">Traveler</span></router-link></li>
+        <li><router-link class="-spot" to ='/spot'><span class="p-category-list-name">spot</span></router-link></li>
+        <li><router-link class="-food" to ='/food'><span class="p-category-list-name">food</span></router-link></li>
+        <li><router-link class="-fashion" to ='/fashion'><span class="p-category-list-name">fashion</span></router-link></li>
+        <li><router-link class="-pickup" to ='/'><span class="p-category-list-name">pickup</span></router-link></li>
         <!--<li><router-link class="-country" to ='/country'><span class="p-category-list-name">Country</span></router-link></li>-->
-        <li><router-link class="-fashion" to ='/fashion'><span class="p-category-list-name">Fashion</span></router-link></li>
-        <li><router-link class="-food" to ='/food'><span class="p-category-list-name">Food</span></router-link></li>
-        <li><router-link class="-spot" to ='/spot'><span class="p-category-list-name">Spot</span></router-link></li>
-        <li><router-link class="-all" to ='/all'><span class="p-category-list-name">All</span></router-link></li>
+        <li><router-link class="-traveler" to ='/traveler'><span class="p-category-list-name">traveler</span></router-link></li>
+        <li><router-link class="-all" to ='/all'><span class="p-category-list-name">all</span></router-link></li>
       </ul>
     </nav>
     <router-view />
