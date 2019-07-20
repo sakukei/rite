480a421124d25095aae50d4cfcc8774be1b1d0ce
Thet Win Aung <minthit21dj@gmail.com>
2019-07-20 17:00:32 +0900

    Add timestamp for index.js

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/front-page.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/front-page.php
@@ -5,11 +5,18 @@
  * @package WordPress
  */
 
-get_header(); ?>
-<div id="app">
+?>
+<!DOCTYPE html>
+<html lang="ja">
 
-</div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" href="./favicon.ico">
+  <title>Rite</title>
+</head>
 
-
-
-  <?php get_footer(); ?>
+<body>
+  <div id="root"></div>
+  <script src="<?php echo get_stylesheet_directory_uri() . '/js/index.js?' . explode(".", microtime(true))[0] ?>"></script>
+</html>
