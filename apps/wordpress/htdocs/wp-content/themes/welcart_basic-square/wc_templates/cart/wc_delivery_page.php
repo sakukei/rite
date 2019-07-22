06f64497a42829a1c014d7df310c28ed74eb5b1c
m1nt1a <tksm.webdesign09@gmail.com>
2019-07-23 00:53:09 +0900

    fix delivery table add class

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/wc_templates/cart/wc_delivery_page.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/wc_templates/cart/wc_delivery_page.php
@@ -41,12 +41,12 @@ usces_delivery_info_script();
 	
 						<?php if( welcart_basic_have_shipped() ) : ?>
 						<table class="customer_form" id="delivery_flag">
-							<tr>
-								<th rowspan="2" scope="row"><?php _e('shipping address', 'usces'); ?></th>
-								<td><input name="delivery[delivery_flag]" type="radio" id="delivery_flag1" onclick="document.getElementById('delivery_table').style.display = 'none';" value="0"<?php if($usces_entries['delivery']['delivery_flag'] == 0) echo ' checked'; ?> onKeyDown="if (event.keyCode == 13) {return false;}" /> <label for="delivery_flag1"><?php _e('same as customer information', 'usces'); ?></label></td>
+							<tr class="tr">
+								<th class="th" rowspan="2" scope="row"><?php _e('shipping address', 'usces'); ?></th>
+								<td class="td"><input name="delivery[delivery_flag]" type="radio" id="delivery_flag1" onclick="document.getElementById('delivery_table').style.display = 'none';" value="0"<?php if($usces_entries['delivery']['delivery_flag'] == 0) echo ' checked'; ?> onKeyDown="if (event.keyCode == 13) {return false;}" /> <label for="delivery_flag1"><?php _e('same as customer information', 'usces'); ?></label></td>
 							</tr>
-							<tr>
-								<td><input name="delivery[delivery_flag]" id="delivery_flag2" onclick="document.getElementById('delivery_table').style.display = 'table'" type="radio" value="1"<?php if($usces_entries['delivery']['delivery_flag'] == 1) echo ' checked'; ?> onKeyDown="if (event.keyCode == 13) {return false;}" /> <label for="delivery_flag2"><?php _e('Chose another shipping address.', 'usces'); ?></label></td>
+							<tr class="tr">
+								<td class="td"><input name="delivery[delivery_flag]" id="delivery_flag2" onclick="document.getElementById('delivery_table').style.display = 'table'" type="radio" value="1"<?php if($usces_entries['delivery']['delivery_flag'] == 1) echo ' checked'; ?> onKeyDown="if (event.keyCode == 13) {return false;}" /> <label for="delivery_flag2"><?php _e('Chose another shipping address.', 'usces'); ?></label></td>
 							</tr>
 						</table>
 						<?php do_action( 'usces_action_delivery_flag' ); ?>
@@ -58,22 +58,22 @@ usces_delivery_info_script();
 	
 						<table class="customer_form" id="time">
 						<?php if( welcart_basic_have_shipped() ) : ?>
-							<tr>
-								<th scope="row"><?php _e('shipping option', 'usces'); ?></th>
-								<td colspan="2"><?php usces_the_delivery_method( $usces_entries['order']['delivery_method'] ); ?></td>
+							<tr class="tr">
+								<th class="th" scope="row"><?php _e('shipping option', 'usces'); ?></th>
+								<td class="td" colspan="2"><?php usces_the_delivery_method( $usces_entries['order']['delivery_method'] ); ?></td>
 							</tr>
-							<tr>
-								<th scope="row"><?php _e('Delivery date', 'usces'); ?></th>
-								<td colspan="2"><?php usces_the_delivery_date( $usces_entries['order']['delivery_date'] ); ?></td>
+							<tr class="tr">
+								<th class="th" scope="row"><?php _e('Delivery date', 'usces'); ?></th>
+								<td class="td" colspan="2"><?php usces_the_delivery_date( $usces_entries['order']['delivery_date'] ); ?></td>
 							</tr>
-							<tr>
-								<th scope="row"><?php _e('Delivery Time', 'usces'); ?></th>
-								<td colspan="2"><?php usces_the_delivery_time( $usces_entries['order']['delivery_time'] ); ?></td>
+							<tr class="tr">
+								<th class="th" scope="row"><?php _e('Delivery Time', 'usces'); ?></th>
+								<td class="td" colspan="2"><?php usces_the_delivery_time( $usces_entries['order']['delivery_time'] ); ?></td>
 							</tr>
 						<?php endif; ?>
-							<tr>
-								<th scope="row"><em><?php _e('*', 'usces'); ?></em><?php _e('payment method', 'usces'); ?></th>
-								<td colspan="2"><?php usces_the_payment_method( $usces_entries['order']['payment_name'] ); ?></td>
+							<tr class="tr">
+								<th class="th" scope="row"><em><?php _e('*', 'usces'); ?></em><?php _e('payment method', 'usces'); ?></th>
+								<td class="td" colspan="2"><?php usces_the_payment_method( $usces_entries['order']['payment_name'] ); ?></td>
 							</tr>
 						</table>
 	
@@ -88,21 +88,21 @@ usces_delivery_info_script();
 	
 						<?php if( welcart_basic_have_dlseller_content() ) : ?>
 						<table class="customer_form" id="dlseller_terms">
-							<tr>
-								<th rowspan="2" scope="row"><em><?php _e('*', 'usces'); ?></em><?php _e('Terms of Use', 'dlseller'); ?></th>
-								<td colspan="2"><div class="dlseller_terms"><?php dlseller_terms(); ?></div></td>
+							<tr class="tr">
+								<th class="th" rowspan="2" scope="row"><em><?php _e('*', 'usces'); ?></em><?php _e('Terms of Use', 'dlseller'); ?></th>
+								<td class="td" colspan="2"><div class="dlseller_terms"><?php dlseller_terms(); ?></div></td>
 							</tr>
-							<tr>
-								<td colspan="2"><label for="terms"><input type="checkbox" name="offer[terms]" id="terms" /><?php _e('Agree', 'dlseller'); ?></label></td>
+							<tr class="tr">
+								<td class="td" colspan="2"><label for="terms"><input type="checkbox" name="offer[terms]" id="terms" /><?php _e('Agree', 'dlseller'); ?></label></td>
 							</tr>
 						</table>
 						<?php endif; ?>
 	
 						<table class="customer_form" id="notes_table">
-							<tr>
+							<tr class="tr">
 								<?php $entry_order_note = ( empty($usces_entries['order']['note']) ) ? apply_filters( 'usces_filter_default_order_note', NULL ) : $usces_entries['order']['note']; ?>
-								<th scope="row"><?php _e('Notes', 'usces'); ?></th>
-								<td colspan="2"><textarea name="offer[note]" id="note" class="notes"><?php echo esc_html($entry_order_note); ?></textarea></td>
+								<th class="th" scope="row"><?php _e('Notes', 'usces'); ?></th>
+								<td class="td" colspan="2"><textarea name="offer[note]" id="note" class="notes"><?php echo esc_html($entry_order_note); ?></textarea></td>
 							</tr>
 						</table>
 	
