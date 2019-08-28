f1f1a8ead5b9a7f59b0d0e8a2192d348271b35b3
m1nt1a <tksm.webdesign09@gmail.com>
2019-08-28 15:37:04 +0900

    add slick cdn

--- apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/header.php
+++ apps/wordpress/htdocs/wp-content/themes/welcart_basic-square/header.php
@@ -13,6 +13,8 @@
   <meta name="format-detection" content="telephone=no"/>
 
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 
   <?php wp_head(); ?>
 </head>
